1bfa1d75ddee92e50be115bc1d6b79cad19fffb4
Iakuro-wc <wcdevcontent@gmail.com>
2021-11-08 08:21:43 -0800

    added toggle functionality to hamburger icon in mobile mode, added vector svg to homepage with showPerson toggle, added show sidebar functionality to sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,15 +3,14 @@ import Links from '../constants/links'
 import Categories from '../components/Categories'
 import { IoMdClose } from 'react-icons/io'
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
-    <aside className={`sidebar`}>
-      <button className="close-btn">
-        {' '}
-        <IoMdClose />{' '}
+    <aside className={`sidebar ${isOpen ? 'showSidebar' : ''}`}>
+      <button className="close-btn" onClick={toggle}>
+        <IoMdClose />
       </button>
       <div className="sidebar-container">
-        <Links className="sidebar-links" />
+        <Links styleClass="sidebar-links" />
       </div>
     </aside>
   )
