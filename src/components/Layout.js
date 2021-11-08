9127cd3a98a9665c38f7327b7ad49da1ef27304b
Iakuro-wc <wcdevcontent@gmail.com>
2021-11-08 06:53:01 -0800

    starter blog tutorial

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {/* <Sidebar /> */}
+      <main>{children}</main>
+      {/* <Footer></Footer> */}
+    </>
+  )
+}
+
+export default Layout
